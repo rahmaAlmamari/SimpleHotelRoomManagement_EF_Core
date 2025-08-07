efc082f3c61798f5e4328d7d07e543335efb9d44
rahmaAlmamari <alr816125@gmail.com>
2025-08-07 12:31:53 +0400

    Adding fields to Guest class

--- SimpleHotelRoomManagement_EF_Core/Models/Guest.cs
+++ SimpleHotelRoomManagement_EF_Core/Models/Guest.cs
@@ -8,5 +8,9 @@ namespace SimpleHotelRoomManagement_EF_Core.Models
 {
     public class Guest
     {
+        public int GuestId { get; set; }
+        public string GuestName { get; set; }
+        public string GuestEmail { get; set; }
+        public string GuestPhoneNumber { get; set; }
     }
 }
