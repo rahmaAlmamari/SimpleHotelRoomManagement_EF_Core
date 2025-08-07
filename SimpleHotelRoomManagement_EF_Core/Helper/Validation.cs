28e8e441af4024a62dd41880651df8117d2a2a8a
rahmaAlmamari <alr816125@gmail.com>
2025-08-07 11:24:21 +0400

    Adding CharValidation method

--- SimpleHotelRoomManagement_EF_Core/Helper/Validation.cs
+++ SimpleHotelRoomManagement_EF_Core/Helper/Validation.cs
@@ -8,5 +8,29 @@ namespace SimpleHotelRoomManagement_EF_Core.Helper
 {
     public static class Validation
     {
+        //1. CharValidation method ...
+        public static char CharValidation(string message)
+        {
+            bool CharFlag;//to handle user char error input ...
+            char CharInput = '0';
+            do
+            {
+                try
+                {
+                    CharFlag = false;
+                    Console.WriteLine($"Enter your {message}:");
+                    CharInput = char.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Your {message} not accepted due to: " + e.Message);
+                    CharFlag = true;
+                }
+
+            } while (CharFlag);
+
+            //to return tne char input ...
+            return CharInput;
+        }
     }
 }
