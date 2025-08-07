11d77444c1b9a3d7d546b8d6a676418c6c49ba44
rahmaAlmamari <alr816125@gmail.com>
2025-08-07 11:25:04 +0400

    Adding StringNamingValidation method

--- SimpleHotelRoomManagement_EF_Core/Helper/Validation.cs
+++ SimpleHotelRoomManagement_EF_Core/Helper/Validation.cs
@@ -32,5 +32,31 @@ namespace SimpleHotelRoomManagement_EF_Core.Helper
             //to return tne char input ...
             return CharInput;
         }
+
+        //2. StringNamingValidation method ...
+        public static string StringNamingValidation(string message)
+        {
+            bool StringNamingFlag;//to handle user StringNaming error input ...
+            string StringNamingInput = "null";
+            do
+            {
+                StringNamingFlag = false;
+                Console.WriteLine($"Enter your {message}:");
+                StringNamingInput = Console.ReadLine();
+                //to check if StringNamingInput has number or not ...
+                bool check_StringNaming = Additional.IsAlpha(StringNamingInput);
+                if (check_StringNaming == false)
+                {
+                    Console.WriteLine($"{message} can not contains number and con not be null ..." +
+                                      "please prass enter key to try again");
+                    Additional.HoldScreen();//just to hold a second ...
+                    StringNamingFlag = true;
+                }
+
+            } while (StringNamingFlag);
+
+            //to return tne char input ...
+            return StringNamingInput;
+        }
     }
 }
