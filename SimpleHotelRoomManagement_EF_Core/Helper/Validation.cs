40d45ffe5c3a77541f6124de72b210db3ed59b81
rahmaAlmamari <alr816125@gmail.com>
2025-08-07 11:53:50 +0400

    Adding UserPhoneNumberValidation method

--- SimpleHotelRoomManagement_EF_Core/Helper/Validation.cs
+++ SimpleHotelRoomManagement_EF_Core/Helper/Validation.cs
@@ -341,5 +341,23 @@ namespace SimpleHotelRoomManagement_EF_Core.Helper
             //to return tne char input ...
             return emailInput;
         }
+        //14. UserPhoneNumberValidation method ...
+        public static int UserPhoneNumberValidation()
+        {
+            bool FalgError = false;
+            int value = 0;
+            do
+            {
+                FalgError = false;
+                value = IntValidation("phone number");
+                //to check if the phone number is 8 digits or not ...
+                if (value < 10000000 || value > 99999999)
+                {
+                    Console.WriteLine("Phone number must be 8 digits.");
+                    FalgError = true; //to handle the error ...
+                }
+            } while (FalgError);
+            return value; // Return the valid phone number
+        }
     }
 }
