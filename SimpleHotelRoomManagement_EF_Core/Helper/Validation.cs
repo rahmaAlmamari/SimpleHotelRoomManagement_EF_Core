b6d622ee3d962c4a1ca72fa5a8d54c51a3f06e25
rahmaAlmamari <alr816125@gmail.com>
2025-08-07 11:36:23 +0400

    Adding IsValidEmail method to check if email in the right format or not

--- SimpleHotelRoomManagement_EF_Core/Helper/Validation.cs
+++ SimpleHotelRoomManagement_EF_Core/Helper/Validation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SimpleHotelRoomManagement_EF_Core.Helper
@@ -307,6 +308,17 @@ namespace SimpleHotelRoomManagement_EF_Core.Helper
                 }
             }
             return IsUnique; // No match
+        }
+
+        //12. IsValidEmail method to check if email in the right format or not ...
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            // Basic but solid regex for general email validation
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
         }
     }
 }
