7b3533f54a178b2aa7019632847441d69314f896
rahmaAlmamari <alr816125@gmail.com>
2025-08-07 10:43:43 +0400

    Add project files.

---
+++ SimpleHotelRoomManagement_EF_Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleHotelRoomManagement_EF_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
