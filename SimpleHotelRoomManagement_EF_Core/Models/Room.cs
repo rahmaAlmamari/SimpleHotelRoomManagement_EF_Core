28e44f2c4b072eaa781382ce8364d23a03ca0e3a
rahmaAlmamari <alr816125@gmail.com>
2025-08-07 12:38:15 +0400

    Adding fields to Room class

--- SimpleHotelRoomManagement_EF_Core/Models/Room.cs
+++ SimpleHotelRoomManagement_EF_Core/Models/Room.cs
@@ -8,5 +8,9 @@ namespace SimpleHotelRoomManagement_EF_Core.Models
 {
     public class Room
     {
+        public int RoomNumber { get; set; }
+        public double RoomDailyPrice { get; set; }
+
+        public bool IsAvailable = true;
     }
 }
