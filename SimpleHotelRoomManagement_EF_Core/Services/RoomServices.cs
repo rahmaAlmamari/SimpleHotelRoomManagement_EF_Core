08261b2f466903d32dfc522fa7108a622084f5ef
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 12:12:50 +0400

    to create private field for the RoomRepositry

--- SimpleHotelRoomManagement_EF_Core/Services/RoomServices.cs
+++ SimpleHotelRoomManagement_EF_Core/Services/RoomServices.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SimpleHotelRoomManagement_EF_Core.Models;
+using SimpleHotelRoomManagement_EF_Core.Repositries;
 
 namespace SimpleHotelRoomManagement_EF_Core.Services
 {
     public class RoomServices
     {
+        //to create private field for the RoomRepositry ...
+        private readonly IRoomRepositry _RoomRepositry;
     }
 }
