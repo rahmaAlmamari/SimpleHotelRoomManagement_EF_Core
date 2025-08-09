999ed8294e1479b8219a8bb35483c6d12d7b5c1a
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 11:34:46 +0400

    Extract IGuestRepositry interface

---
+++ SimpleHotelRoomManagement_EF_Core/Repositries/IGuestRepositry.cs
@@ -0,0 +1,15 @@
+﻿using SimpleHotelRoomManagement_EF_Core.Models;
+
+namespace SimpleHotelRoomManagement_EF_Core.Repositries
+{
+    public interface IGuestRepositry
+    {
+        void AddGuest(Guest guest);
+        void DeleteGuest(int id);
+        List<Guest> GetAllGuests();
+        Guest GetGuestById(int id);
+        void UpdateGuestEmail(int id, string newEmail);
+        void UpdateGuestName(int id, string newName);
+        void UpdateGuestPhoneNumber(int id, string newPhoneNumber);
+    }
+}
