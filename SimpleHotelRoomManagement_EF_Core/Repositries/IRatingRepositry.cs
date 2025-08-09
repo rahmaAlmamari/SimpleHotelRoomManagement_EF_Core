35eeaaf1a645421dcafdd73216673e1422c3162b
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 12:01:08 +0400

    Extract IRatingRepositry interface

---
+++ SimpleHotelRoomManagement_EF_Core/Repositries/IRatingRepositry.cs
@@ -0,0 +1,17 @@
+﻿using SimpleHotelRoomManagement_EF_Core.Models;
+
+namespace SimpleHotelRoomManagement_EF_Core.Repositries
+{
+    public interface IRatingRepositry
+    {
+        void AddRating(Rating rating);
+        void DeleteRating(int id);
+        List<Rating> GetAllRatings();
+        Rating GetRatingById(int id);
+        List<Rating> GetRatingsByGuestId(int guestId);
+        List<Rating> GetRatingsByRoomNumber(int roomNumber);
+        void UpdateRatingComment(int id, string newComment);
+        void UpdateRatingDate(int id, DateTime newDate);
+        void UpdateRatingScore(int id, int newScore);
+    }
+}
