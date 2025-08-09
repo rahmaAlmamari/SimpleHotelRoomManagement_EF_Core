049afd3da95f75747964d6fd25d8a0d71a8125c4
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 11:36:10 +0400

    Adding RoomRepositry class

---
+++ SimpleHotelRoomManagement_EF_Core/Repositries/RoomRepositry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelRoomManagement_EF_Core.Repositries
+{
+    class RoomRepositry
+    {
+    }
+}
