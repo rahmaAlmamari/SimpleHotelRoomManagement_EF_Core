64177fb50d291126c26f121d131924a73598a4b0
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 10:08:22 +0400

    Adding fields to booking class

--- SimpleHotelRoomManagement_EF_Core/Models/Booking.cs
+++ SimpleHotelRoomManagement_EF_Core/Models/Booking.cs
@@ -8,5 +8,12 @@ namespace SimpleHotelRoomManagement_EF_Core.Models
 {
     public class Booking
     {
+        public int BookingId { get; set; }
+        public int RoomNumber { get; set; }
+        public int GuestId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public double TotalPrice { get; set; }
+
     }
 }
