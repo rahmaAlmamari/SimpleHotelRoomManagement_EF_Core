5c6851b6d4ca64046f165405905215b1e76ba140
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 12:01:44 +0400

    Set RatingRepositry class to implement IRatingRepositry interface

--- SimpleHotelRoomManagement_EF_Core/Repositries/RatingRepositry.cs
+++ SimpleHotelRoomManagement_EF_Core/Repositries/RatingRepositry.cs
@@ -7,7 +7,7 @@ using SimpleHotelRoomManagement_EF_Core.Models;
 
 namespace SimpleHotelRoomManagement_EF_Core.Repositries
 {
-    public class RatingRepositry
+    public class RatingRepositry : IRatingRepositry
     {
         //to create private field for the DbContext ...
         private readonly HotelDbContext _context;
