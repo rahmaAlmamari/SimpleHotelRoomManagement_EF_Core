b1e15a6ff5aec4b4d794147132c7a56b0104ad19
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 10:20:45 +0400

    Adding data annotations to guest class

--- SimpleHotelRoomManagement_EF_Core/Models/Guest.cs
+++ SimpleHotelRoomManagement_EF_Core/Models/Guest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,17 @@ namespace SimpleHotelRoomManagement_EF_Core.Models
 {
     public class Guest
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int GuestId { get; set; }
+        [Required]
+        [MaxLength(20)]
         public string GuestName { get; set; }
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$")]
         public string GuestEmail { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{8}$")]
         public string GuestPhoneNumber { get; set; }
     }
 }
