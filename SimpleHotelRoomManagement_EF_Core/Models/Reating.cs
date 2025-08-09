0df4146cbfb840feed4cd5208077130a5e8f60b7
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 10:24:57 +0400

    Adding data annotations to rating class

--- SimpleHotelRoomManagement_EF_Core/Models/Reating.cs
+++ SimpleHotelRoomManagement_EF_Core/Models/Reating.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +10,22 @@ namespace SimpleHotelRoomManagement_EF_Core.Models
 {
     public class Rating
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int RatingId { get; set; }
 
         public int GuestId { get; set; }
         public int RoomNumber { get; set; }
+
+        [Required]
+        [Range(1, 5)] // Assuming a rating scale from 1 to 5
         public int Score { get; set; } // Assuming rating is an integer value (e.g., 1 to 5)
+
+        [Required]
+        [MaxLength(500)] // Optional comment, can be up to 500 characters
         public string Comment { get; set; } // Optional comment about the rating
+
+        [Required]
         public DateTime RatingDate { get; set; } // Date when the rating was given
     }
 }
