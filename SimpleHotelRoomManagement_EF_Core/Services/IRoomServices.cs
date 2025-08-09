0464517c249d3777cd33acfcf81d1b5bb49c3caa
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 12:17:08 +0400

    Extract IRoomServices interface

---
+++ SimpleHotelRoomManagement_EF_Core/Services/IRoomServices.cs
@@ -0,0 +1,14 @@
+﻿using SimpleHotelRoomManagement_EF_Core.Models;
+
+namespace SimpleHotelRoomManagement_EF_Core.Services
+{
+    public interface IRoomServices
+    {
+        void AddRoom(Room room);
+        void DeleteRoom(int roomNumber);
+        List<Room> GetAllRooms();
+        Room GetRoomByNumber(int roomNumber);
+        void UpdateRoomAvailability(int roomNumber, bool isAvailable);
+        void UpdateRoomDailyPrice(int roomNumber, double newDailyPrice);
+    }
+}
