8da355b9d219d288f1a70486aa32e38d89611c75
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 11:53:34 +0400

    Set BookingRepositry class to implement IBookingRepositry interface

--- SimpleHotelRoomManagement_EF_Core/Repositries/BookingRepositry.cs
+++ SimpleHotelRoomManagement_EF_Core/Repositries/BookingRepositry.cs
@@ -7,7 +7,7 @@ using SimpleHotelRoomManagement_EF_Core.Models;
 
 namespace SimpleHotelRoomManagement_EF_Core.Repositries
 {
-    public class BookingRepositry
+    public class BookingRepositry : IBookingRepositry
     {
         //to create private field for the DbContext ...
         private readonly HotelDbContext _context;
