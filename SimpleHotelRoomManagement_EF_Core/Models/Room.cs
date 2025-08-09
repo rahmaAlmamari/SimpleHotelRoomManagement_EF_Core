e557c36cc67c2b2311c72e62143a55add0c3afe2
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 10:21:47 +0400

    Adding data annotations to room class

--- SimpleHotelRoomManagement_EF_Core/Models/Room.cs
+++ SimpleHotelRoomManagement_EF_Core/Models/Room.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,12 @@ namespace SimpleHotelRoomManagement_EF_Core.Models
 {
     public class Room
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int RoomNumber { get; set; }
+        [Required]
         public double RoomDailyPrice { get; set; }
-
+        [Required]
         public bool IsAvailable = true;
     }
 }
