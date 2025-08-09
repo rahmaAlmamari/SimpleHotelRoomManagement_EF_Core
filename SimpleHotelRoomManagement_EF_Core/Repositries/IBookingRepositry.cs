1653864122b5affc257733c7e4578623bbae54bd
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 11:53:05 +0400

    Extract IBookingRepositry interface

---
+++ SimpleHotelRoomManagement_EF_Core/Repositries/IBookingRepositry.cs
@@ -0,0 +1,17 @@
+﻿using SimpleHotelRoomManagement_EF_Core.Models;
+
+namespace SimpleHotelRoomManagement_EF_Core.Repositries
+{
+    public interface IBookingRepositry
+    {
+        void AddBooking(Booking booking);
+        void DeleteBooking(int bookingId);
+        List<Booking> GetAllBookings();
+        Booking GetBookingById(int id);
+        List<Booking> GetBookingsByGuestId(int guestId);
+        List<Booking> GetBookingsByRoomNumber(int roomNumber);
+        void UpdateBookingCheckInDate(int bookingId, DateTime newCheckInDate);
+        void UpdateBookingCheckOutDate(int bookingId, DateTime newCheckOutDate);
+        void UpdateBookingTotalPrice(int bookingId, double newTotalPrice);
+    }
+}
