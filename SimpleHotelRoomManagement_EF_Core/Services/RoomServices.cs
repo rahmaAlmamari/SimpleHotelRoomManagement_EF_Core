8e85946517cf094aba3357400fe322f6c9d7f606
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 12:11:55 +0400

    Set RoomServices class to use SimpleHotelRoomManagement_EF_Core.Models

--- SimpleHotelRoomManagement_EF_Core/Services/RoomServices.cs
+++ SimpleHotelRoomManagement_EF_Core/Services/RoomServices.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleHotelRoomManagement_EF_Core.Models;
 
 namespace SimpleHotelRoomManagement_EF_Core.Services
 {
