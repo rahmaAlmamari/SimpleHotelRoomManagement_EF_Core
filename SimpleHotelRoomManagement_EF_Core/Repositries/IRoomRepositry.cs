705cd1612518b80e2eb2ce619c2fffa460225db8
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 11:43:17 +0400

    Extract IRoomRepositry interface

---
+++ SimpleHotelRoomManagement_EF_Core/Repositries/IRoomRepositry.cs
@@ -0,0 +1,14 @@
+﻿using SimpleHotelRoomManagement_EF_Core.Models;
+
+namespace SimpleHotelRoomManagement_EF_Core.Repositries
+{
+    public interface IRoomRepositry
+    {
+        void AddRoom(Room room);
+        void DeleteRoom(int roomNumber);
+        List<Room> GetAllRooms();
+        Room GetRoomByNumber(int roomNumber);
+        void UpdateRoomAvailability(int roomNumber, bool isAvailable);
+        void UpdateRoomDailyPrice(int roomNumber, double newDailyPrice);
+    }
+}
