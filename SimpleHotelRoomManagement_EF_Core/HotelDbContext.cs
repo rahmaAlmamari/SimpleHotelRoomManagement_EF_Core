4ccb4989b360f47f839e2a5b1bbf694b301adff3
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 11:02:26 +0400

    Finishing HotelDbContext file

--- SimpleHotelRoomManagement_EF_Core/HotelDbContext.cs
+++ SimpleHotelRoomManagement_EF_Core/HotelDbContext.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleHotelRoomManagement_EF_Core.Models;
+
 
 namespace SimpleHotelRoomManagement_EF_Core
 {
@@ -14,5 +16,39 @@ namespace SimpleHotelRoomManagement_EF_Core
         {
             optionsBuilder.UseSqlServer("Data Source=LAPTOP-IUF9HHIH;Initial Catalog=HotelDB;Integrated Security=True;TrustServerCertificate=True");
         }
+
+        // DbSet properties for each model class
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+
+        // Override OnModelCreating to configure relationships and constraints
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    // Booking relationship
+        //    modelBuilder.Entity<Booking>()
+        //        .HasOne(b => b.Guest)
+        //        .WithMany(g => g.GuestBooking)
+        //        .HasForeignKey(b => b.GuestId);
+
+        //    modelBuilder.Entity<Booking>()
+        //        .HasOne(b => b.Room)
+        //        .WithMany(r => r.RoomBooking)
+        //        .HasForeignKey(b => b.RoomNumber);
+
+        //    // Rating relationship
+        //    modelBuilder.Entity<Rating>()
+        //        .HasOne(r => r.Guest)
+        //        .WithMany(g => g.GuestRating)
+        //        .HasForeignKey(r => r.GuestId);
+
+        //    modelBuilder.Entity<Rating>()
+        //        .HasOne(r => r.Room)
+        //        .WithMany(ro => ro.RoomRating)
+        //        .HasForeignKey(r => r.RoomNumber);
+        //}
+
+
     }
 }
