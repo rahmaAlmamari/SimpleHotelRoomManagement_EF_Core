81e1f6146bb8fea29de6232d31699f12637c8259
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 10:11:49 +0400

    Adding fields to rating class

--- SimpleHotelRoomManagement_EF_Core/Models/Reating.cs
+++ SimpleHotelRoomManagement_EF_Core/Models/Reating.cs
@@ -8,5 +8,12 @@ namespace SimpleHotelRoomManagement_EF_Core.Models
 {
     public class Reating
     {
+        public int ReatingId { get; set; }
+
+        public int GuestId { get; set; }
+        public int RoomNumber { get; set; }
+        public int Score { get; set; } // Assuming rating is an integer value (e.g., 1 to 5)
+        public string Comment { get; set; } // Optional comment about the rating
+        public DateTime RatingDate { get; set; } // Date when the rating was given
     }
 }
