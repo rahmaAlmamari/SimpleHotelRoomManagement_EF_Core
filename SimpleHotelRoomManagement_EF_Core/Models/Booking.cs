d8e0f03111b13145a6ec02e29efc6d751e921315
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 10:23:45 +0400

    Adding data annotations for booking class

--- SimpleHotelRoomManagement_EF_Core/Models/Booking.cs
+++ SimpleHotelRoomManagement_EF_Core/Models/Booking.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,16 @@ namespace SimpleHotelRoomManagement_EF_Core.Models
 {
     public class Booking
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int BookingId { get; set; }
         public int RoomNumber { get; set; }
         public int GuestId { get; set; }
+        [Required]
         public DateTime CheckInDate { get; set; }
+        [Required]
         public DateTime CheckOutDate { get; set; }
+        [Required]
         public double TotalPrice { get; set; }
 
     }
