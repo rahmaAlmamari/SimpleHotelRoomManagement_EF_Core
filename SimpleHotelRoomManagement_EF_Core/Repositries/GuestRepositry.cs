e6d6c4d268068bef7378831bc30ac75690e58565
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 11:28:23 +0400

    to create private field for the DbContext

--- SimpleHotelRoomManagement_EF_Core/Repositries/GuestRepositry.cs
+++ SimpleHotelRoomManagement_EF_Core/Repositries/GuestRepositry.cs
@@ -8,5 +8,7 @@ namespace SimpleHotelRoomManagement_EF_Core.Repositries
 {
     public class GuestRepositry
     {
+        //to create private field for the DbContext ...
+        private readonly HotelDbContext _context;
     }
 }
