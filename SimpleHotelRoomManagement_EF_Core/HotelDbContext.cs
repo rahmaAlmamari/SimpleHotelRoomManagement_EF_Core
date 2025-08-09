cd81a4b6978b170ab4cfde15a71face9b4541b18
rahmaAlmamari <alr816125@gmail.com>
2025-08-09 10:55:51 +0400

    Adding OnConfiguring method to set the connection string

--- SimpleHotelRoomManagement_EF_Core/HotelDbContext.cs
+++ SimpleHotelRoomManagement_EF_Core/HotelDbContext.cs
@@ -9,5 +9,10 @@ namespace SimpleHotelRoomManagement_EF_Core
 {
     public class HotelDbContext : DbContext
     {
+        //to override the OnConfiguring method to set the connection string ...
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-IUF9HHIH;Initial Catalog=HotelDB;Integrated Security=True;TrustServerCertificate=True");
+        }
     }
 }
